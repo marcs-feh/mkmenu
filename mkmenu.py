0a6247ccd8de5528b1ae442dfd23f9c56d45df70
marcs-feh <marcosfehlauer@protonmail.com>
2021-11-22 23:17:15 -0300

    added item parsing

--- mkmenu.py
+++ mkmenu.py
@@ -11,14 +11,46 @@ def filestr(path:str) -> str:
 
     return fstr
 
+def linedepth(line:str) -> int:
+    depth = 0
+    for char in line:
+        if char == ' ':
+            depth += 1
+        else:
+            break
+    
+    return depth // 2
+
+def parseitem(line:str):
+    item = {
+        'name':'',
+        'exec':''
+    }
+    l = len(line)
+    i = 0
+    while i < l:
+        if line[i] == ':':
+            i += 1
+            while i < l:
+                item['exec'] += line[i]
+                i += 1
+            break
+        else:
+            item['name'] += line[i]
+        i += 1
+    
+    return item
+
 def usage():
-    print(  'USAGE: mkmenu [MENUFILE]')
+    print('USAGE: mkmenu [MENUFILE]')
 
 def main(argc:int, argv:list):
     if argc < 2:
         usage()
         exit(1)
-    loadfile(argv[1])
+
+    fbuffer = filestr(argv[1])
+    
 
 if __name__ == '__main__':
     argc = len(argv)
