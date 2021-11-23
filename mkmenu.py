a9f17a28c1d3bfa8243cb78ac34ba7d8ea72a244
marcs-feh <marcosfehlauer@protonmail.com>
2021-11-23 12:43:06 -0300

    item parsing and rm blanklines

--- mkmenu.py
+++ mkmenu.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 from sys import argv, exit
+import re
 
 def filestr(path:str) -> str:
     try:
@@ -9,7 +10,17 @@ def filestr(path:str) -> str:
         print(f'mkmenu: no such file or directory: \'{path}\'')
         exit(1)
 
-    return fstr
+    return fstr+'\n'
+
+def rmws(txt:str):
+    s = txt
+    leadingws = re.compile(r'^\s+')
+    blankline = re.compile(r'^\s*$')
+
+    s = leadingws.sub('', s)
+    s = blankline.sub('', s)
+
+    return s
 
 def linedepth(line:str) -> int:
     depth = 0
@@ -23,8 +34,9 @@ def linedepth(line:str) -> int:
 
 def parseitem(line:str):
     item = {
-        'name':'',
-        'exec':''
+        'label':'',
+        'exec':'',
+        'depth':linedepth(line)
     }
     l = len(line)
     i = 0
@@ -36,10 +48,21 @@ def parseitem(line:str):
                 i += 1
             break
         else:
-            item['name'] += line[i]
+            item['label'] += line[i]
         i += 1
     
+    item['label'] = rmws(item['label'])
+
+    if item['label'] == '':
+        return
+
     return item
+
+def mkxml(menu) -> str:
+    xml = '<?xml version="1.0" encoding="UTF-8">\n<openbox_menu xmlns="http://openbox.org/3.4/menu">\n'
+
+    xml += '</openbox_menu>\n'
+    return
 
 def usage():
     print('USAGE: mkmenu [MENUFILE]')
@@ -50,6 +73,10 @@ def main(argc:int, argv:list):
         exit(1)
 
     fbuffer = filestr(argv[1])
+    fbuffer = fbuffer.split('\n')
+    for l in fbuffer:
+        print(parseitem(l))
+    
     
 
 if __name__ == '__main__':
